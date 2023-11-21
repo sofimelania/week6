15a67b4498ac5228849591e34f82a2df6349b70b
sofimelania <ssirnik1987@gmail.com>
2023-11-21 13:32:52 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,105 @@
+export const data = [
+
+  {
+
+    id: 1,
+
+    gift: "Barbie doll",
+
+    image: "https://i5.walmartimages.com/seo/2023-Holiday-Barbie-Doll-Seasonal-Collector-Gift-Golden-Gown-and-Light-Brown-Hair_1e89f8a2-cd7f-46c3-ada2-44eeb2d7bb82.c2ddb7dd9f275edf06b1288ea87d1ca1.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 2,
+
+    gift: "Air Fryer",
+
+    image: "https://i5.walmartimages.com/seo/Ninja-4QT-Air-Fryer-Black-AF100WM_0907e796-99b3-4021-938b-9a2a44a12ab2.13d1211bf2d0ac49e5bd757201e593c7.png?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 3,
+
+    gift: "Apple AirPods",
+
+    image: "https://i5.walmartimages.com/seo/Restored-Apple-AirPods-Pro-2-White-In-Ear-Headphones-MQD83AM-A-Refurbished_95e6f27f-61a9-4b18-903d-d724d067ce51.d0c0d62cc1b1337b847a6a2412149a26.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 4,
+
+    gift: "Apple MacBook",
+
+    image: "https://i5.walmartimages.com/seo/Apple-MacBook-Pro-13-3-Laptop-Intel-Core-i5-2-50GHz-4GB-RAM-500GB-HDD-MD101LL-A_a52a7135-0aff-4d56-8840-10c2f43d947a_1.3bbd8baa7aea4da294c8f11846b2ef85.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 5,
+
+    gift: "Tool Set",
+
+    image: "https://i5.walmartimages.com/seo/Ktaxon-198-Piece-Tool-Set-General-Household-Hand-Tool-Kit-W-Plastic-Storage-Case_1c954e2f-546d-4fd2-ad1f-038a5714313c.fdbdd63f08ee01973cc42eeb29ba4bf0.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+{
+
+    id: 6,
+
+    gift: " Perfume",
+
+    image: "https://i5.walmartimages.com/seo/Versace-Bright-Crystal-Eau-De-Toilette-Spray-Perfume-for-Women-3-oz_a6cebddc-c963-4b55-ae61-6e68e5012f0f.26b3e9eb53ce3cc42d4c09077502e664.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+  {
+
+    id: 7,
+
+    gift: " Book",
+
+    image: "https://i5.walmartimages.com/seo/Friends-Lovers-and-the-Big-Terrible-Thing-A-Memoir-Paperback-9781250866455_1fb68449-7d63-46e5-b92b-f5cbe9333ef1.27d3013dedb03844d448387d8087502e.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 8,
+
+    gift: " Barbie Dreamhouse",
+
+    image: "https://i5.walmartimages.com/asr/4b88132a-4fb6-421f-8ae2-cc0872c9c443.a3bace8fad0b731bc8933392a223c044.jpeg?odnHeight=117&odnWidth=117&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 9,
+
+    gift: " Makeup Kit",
+
+    image: "https://i5.walmartimages.com/asr/40bf64ee-127d-4365-88b2-94f90572798a_1.0a97291823a328026d9362be1fbcc518.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+  {
+
+    id: 10,
+
+    gift: " Lamborghini 12 V Powered Ride on Cars",
+
+    image: "https://i5.walmartimages.com/seo/Lamborghini-12-V-Powered-Ride-on-Cars-Remote-Control-Battery-Powered-Pink_53f36c47-0e40-4454-9b8d-c16778242c4a.f1aadcb382db2ad36f4466cc84be4794.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+
+  },
+
+
+
+
+]
